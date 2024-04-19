fd7bb515166289fa069144eb2f6fabb4260a6435
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-04-19 18:04:15 -0300

    feat: create Title component for section titles

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,18 +1,22 @@
 import { CategoriesData } from "../../data/Categories/categories";
+import { Title } from "../Title";
 import { CategoryCard } from "./CategoryCard";
 
 const Categories = () => {
     return (
-        <ul>
-            {CategoriesData.map((category, index) => (
-                <CategoryCard
-                    key={index}
-                    name={category.name}
-                    imageSrc={category.image}
-                    imageAlt={category.imgAlt}
-                />
-            ))}
-        </ul>
+        <section>
+            <Title text="Compre por categoria" classProp="" />
+            <ul>
+                {CategoriesData.map((category, index) => (
+                    <CategoryCard
+                        key={index}
+                        name={category.name}
+                        imageSrc={category.image}
+                        imageAlt={category.imgAlt}
+                    />
+                ))}
+            </ul>
+        </section>
     );
 };
 
