b1f72bf7de4aa826412d10c422e5656cb9c97632
Gabriel (Notebook) <itsgabrielmontenegro@gmail.com>
2024-04-19 23:40:38 -0300

    fix: update images path

--- src/components/Header/Icons/index.tsx
+++ src/components/Header/Icons/index.tsx
@@ -2,16 +2,13 @@ const Icons = () => {
     return (
         <div>
             <button>
-                <img src="/public/assets/icons/header/userIcon.svg" alt="" />
+                <img src="assets/icons/header/userIcon.svg" alt="" />
             </button>
             <button>
-                <img src="/public/assets/icons/header/heartIcon.svg" alt="" />
+                <img src="assets/icons/header/heartIcon.svg" alt="" />
             </button>
             <button>
-                <img
-                    src="/public/assets/icons/header/shoppingCartIcon.svg"
-                    alt=""
-                />
+                <img src="assets/icons/header/shoppingCartIcon.svg" alt="" />
             </button>
         </div>
     );
